8eee0caa350a4fcf539cca5c3bf157b3ceab1af8
Bradley Bishop <bradley.bishop@encore.tech>
2024-01-24 18:35:52 -0500

    Adding details to package.json. Added configuration items and made sure that they will work at the file level.

--- src/extension.ts
+++ src/extension.ts
@@ -8,12 +8,50 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-hiera-eyaml" is now active!');
+	console.log('Congratulations, your extension "hiera-eyaml" is now active!');
+	// const eyamlSettings = vscode.workspace.getConfiguration('EncoreTechnologies.heira-eyaml');
+	let config = vscode.workspace.getConfiguration('EncoreTechnologies.heira-eyaml');
+	if (vscode.workspace.workspaceFolders) {
+		console.log('loading workspace folder settings');
+		console.log('vscode.workspace.workspaceFolders: ', vscode.workspace.workspaceFolders);
+		config = vscode.workspace.getConfiguration('EncoreTechnologies.heira-eyaml', vscode.workspace.workspaceFolders[3].uri);
+	}
+	console.log('config: ', config);
+	const eyamlPath = config.get('eyamlPath');
+	const publicKeyPath = config.get('publicKeyPath');
+	const privateKeyPath = config.get('privateKeyPath');
+	// const eyamlPath = eyamlSettings['eyamlPath'];
+	// const publicKeyPath = eyamlSettings['publicKeyPath'];
+	// const privateKeyPath = eyamlSettings['privateKeyPath'];
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-hiera-eyaml.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('hiera-eyaml.helloWorld', () => {
+		console.log('hiera-eyaml.helloWorld command has been run!');
+
+		let config = vscode.workspace.getConfiguration('EncoreTechnologies.heira-eyaml');
+		if (vscode.workspace.workspaceFolders && vscode.window.activeTextEditor) {
+			console.log('There is an active text editor');
+			console.log('There are multiple workspace folders');
+			const currentFileUri = vscode.window.activeTextEditor.document.uri;
+			vscode.workspace.workspaceFolders.forEach(folder => {
+				if (currentFileUri.path.includes(folder.uri.path)) {
+					console.log('The current file is in the workspace folder: ', folder.name);
+					config = vscode.workspace.getConfiguration('EncoreTechnologies.heira-eyaml', folder.uri);
+					return true;
+				}
+			});
+		}
+
+		console.log('config: ', config);
+		const eyamlPath = config.get('eyamlPath');
+		const publicKeyPath = config.get('publicKeyPath');
+		const privateKeyPath = config.get('privateKeyPath');
+		
+		console.log('eyamlPath: ', eyamlPath);
+		console.log('publicKeyPath: ', publicKeyPath);
+		console.log('privateKeyPath: ', privateKeyPath);
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from hiera-eyaml!');
